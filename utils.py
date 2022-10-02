4e2e05989051eb9e3ee7e7978a0adbed8927b0f6
Moxitron <70322560+moxitron@users.noreply.github.com>
2022-10-02 16:54:35 +0330

    FIX: fixed bug at util.py

--- utils.py
+++ utils.py
@@ -6,7 +6,6 @@ TAU = 1e-3
 MINIBATCH_SIZE = 64
 EPSILON_DECAY_RATE = 0.995
 MIN_EPSILON = 0.01
-
 def update_target_network(q_network , target_q_network) : 
 	for q_weight , target_q_weight in zip(q_network , target_q_network) :
 		target_q_weight.assign(TAU * q_weight + (1 - TAU) * target_q_weight) 
@@ -23,18 +22,24 @@ def check_update_conditions(timestep , num_steps_per_update , len_buffer) :
 	return (timestep + 1) % num_steps_per_update == 0 and \
 			len_buffer > MINIBATCH_SIZE 
 
-def get_expriences(memory_buffer) : 
+def get_experiences(memory_buffer) : 
 	
-	expriences = random.sample(memory_buffer , MINIBATCH_SIZE) 
-    states = tf.convert_to_tensor(np.array([e.state for e in experiences if e is not None]),dtype=tf.float32)                                         
-    actions = tf.convert_to_tensor(np.array([e.action for e in experiences if e is not None]), dtype=tf.float32)                                      
-    rewards = tf.convert_to_tensor(np.array([e.reward for e in experiences if e is not None]), dtype=tf.float32)                                      
-    next_states = tf.convert_to_tensor(np.array([e.next_state for e in experiences if e is not None]),dtype=tf.float32)                               
-    done_vals = tf.convert_to_tensor(np.array([e.done for e in experiences if e is not None]).astype(np.uint8),                                       
-                                     dtype=tf.float32)                                                                                                
+	experiences = random.sample(memory_buffer , MINIBATCH_SIZE) 
+	states = tf.convert_to_tensor(np.array([e.state for e in experiences if e is not None]),dtype=tf.float32)                                         
+	actions = tf.convert_to_tensor(np.array([e.action for e in experiences if e is not None]), dtype=tf.float32)                                      
+	rewards = tf.convert_to_tensor(np.array([e.reward for e in experiences if e is not None]), dtype=tf.float32)                                      
+	next_states = tf.convert_to_tensor(np.array([e.next_state for e in experiences if e is not None]),dtype=tf.float32)                               
+	done_vals = tf.convert_to_tensor(np.array([e.done for e in experiences if e is not None]).astype(np.uint8),                                       
+							dtype=tf.float32)                                                                                                
 
-    return (states, actions, rewards, next_states, done_vals)                                                                                         
+	return (states, actions, rewards, next_states, done_vals)                                                                                         
 
 def get_new_epsilon(epsilon) : 
 	return max(MIN_EPSILON , EPSILON_DECAY_RATE * epsilon) 
                                                                  
+
+def print_episode_info(episode , latest_avg , num_avg_points) : 
+		
+	end = "\n" if (episode + 1) % 100 == 0 else "\r" 
+	print(f"Episode {episode} | Total point average of the last 100 episodes: {latest_avg}" , end=end) 
+
