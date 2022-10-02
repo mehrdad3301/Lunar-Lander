3c42ccaf297a80626dd6c47d201f9973d61c8517
Moxitron <70322560+moxitron@users.noreply.github.com>
2022-10-02 16:19:32 +0330

    ADD: added check_update_conditions method in utils.py

--- utils.py
+++ utils.py
@@ -2,6 +2,7 @@ import numpy as np
 import tensorflow as tf 
 
 TAU = 1e-3
+MINIBATCH_SIZE = 64
 
 def update_target_network(q_network , target_q_network) : 
 	for q_weight , target_q_weight in zip(q_network , target_q_network) :
@@ -13,4 +14,9 @@ def get_action(q_values , epsilon) :
 	if np.random.rand() < epsilon : 
 		return np.random.randint(q_values.shape[1]) 
 	return np.argmax(q_values.numpy()[0]) 
+
+def check_update_conditions(timestep , num_steps_per_update , len_buffer) : 
+
+	return (timestep + 1) % num_steps_per_update == 0 and \
+			len_buffer > MINIBATCH_SIZE 
 
